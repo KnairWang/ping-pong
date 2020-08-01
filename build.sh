2b08ca069c3286aaaba7c24435b08b384d4ba67f
Knair WANG <knairwang@outlook.com>
2020-08-01 19:16:14 +0800

    update

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 echo "start to build ping-pong"
-
 cargo build --release --target=x86_64-unknown-linux-musl
-
 cp ./target/x86_64-unknown-linux-musl/ping-pong ./docker/
-
 echo "finish building ping-pong"
